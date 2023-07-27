1bf8c37f085b287d986dfdd1ebd34c20d9c0d897
JamJarLid <espen.lever@gmail.com>
2023-07-27 11:24:59 +0200

    news article initial styling

--- src/News.jsx
+++ src/News.jsx
@@ -7,6 +7,18 @@ export default function News() {
     <>
       <h2>ADOVEO DevOps Team</h2>
       <h2>News</h2>
+      <div class="news-feed">
+        <div class="news-article">
+          <h2>News Title</h2>
+          <div class="news-date">
+            <p>2023-07-27</p>
+          </div>
+          <p class="news-ingress">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+            facere?
+          </p>
+        </div>
+      </div>
     </>
   );
 }

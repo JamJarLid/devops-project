6e0de30a97bc08d7103ad518bcea86422c4be8eb
JamJarLid <espen.lever@gmail.com>
2023-08-01 17:53:44 +0200

    className

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,13 +5,13 @@ export default function AboutUs() {
 
   return (
     <>
-      <h1 class="team-title">Möt teamet</h1>
-      <div class="profile-list">
-        <div class="profile-1">
+      <h1 className="team-title">Möt teamet</h1>
+      <div className="profile-list">
+        <div className="profile-1">
           <img src="images/profile picture.png"></img>
-          <h2 class="profile-name">Espen Lever</h2>
-          <h3 class="profile-title">Team Lead</h3>
-          <p class="profile-description">
+          <h2 className="profile-name">Espen Lever</h2>
+          <h3 className="profile-title">Team Lead</h3>
+          <p className="profile-description">
             SCRUM Master / Main contact with stakeholders. Assisting the
             development teams in their progress.
           </p>
@@ -19,9 +19,9 @@ export default function AboutUs() {
 
         <div className="profile-2">
           <img src="images/profile picture.png"></img>
-          <h2 class="profile-name">Harisha Gajavilli</h2>
-          <h3 class="profile-title">Lead Frontend Developer</h3>
-          <p class="profile-description">
+          <h2 className="profile-name">Harisha Gajavilli</h2>
+          <h3 className="profile-title">Lead Frontend Developer</h3>
+          <p className="profile-description">
             Responsible for the design and development of the front-end features
             of the application. Frequently in contact with the SCRUM Master and
             the clients to ensure the product matches the clients’
@@ -31,11 +31,11 @@ export default function AboutUs() {
 
         <div className="profile-1">
           <img src="images/profile picture.png"></img>
-          <h2 class="profile-name">Simon Fröjd</h2>
-          <h3 class="profile-title">
+          <h2 className="profile-name">Simon Fröjd</h2>
+          <h3 className="profile-title">
             Lead Backend Developer/ Security Co-lead
           </h3>
-          <p class="profile-description">
+          <p className="profile-description">
             Responsible for the design and development of the backend (API and
             database structure). Being lead on the API also gives him, in
             collaboration with Andrea, responsibility for the security of the
@@ -45,9 +45,9 @@ export default function AboutUs() {
 
         <div className="profile-2">
           <img src="images/profile picture.png"></img>
-          <h2 class="profile-name">Andréa Bolin*</h2>
-          <h3 class="profile-title">Lead Cloud Engineer/ Security Co-lead</h3>
-          <p class="profile-description">
+          <h2 className="profile-name">Andréa Bolin*</h2>
+          <h3 className="profile-title">Lead Cloud Engineer/ Security Co-lead</h3>
+          <p className="profile-description">
             Responsible for the Deployment of the project into the cloud
             service. Working closely together with Christoffer to deploy the
             project into the cloud server used for the production environment of
@@ -58,9 +58,9 @@ export default function AboutUs() {
 
         <div className="profile-1">
           <img src="images/profile picture.png"></img>
-          <h2 class="profile-name">Christoffer Danielsson*</h2>
-          <h3 class="profile-title">Lead Testing/Integration Engineer</h3>
-          <p class="profile-description">
+          <h2 className="profile-name">Christoffer Danielsson*</h2>
+          <h3 className="profile-title">Lead Testing/Integration Engineer</h3>
+          <p className="profile-description">
             Responsible for the CI of the project. Leading in configuring the
             workflows for integration, as well as designing the automated tests.
           </p>

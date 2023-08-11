5de6620a1073461476c07932df829786a11afc4a
harisha7 <harisha.srinivas74@gmail.com>
2023-08-11 13:33:41 +0200

    fixed the calendar

--- src/calendar/calendarfunction.jsx
+++ src/calendar/calendarfunction.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function calendarFunction() {
-  function viewCalendar() {}
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth());
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
 
-  function changeMonth() {}
+  function viewCalendar(month, year) {
+    setSelectedMonth(month);
+    setSelectedYear(year);
+  }
 
-  return <div></div>;
+  function changeMonth(step) {
+    setSelectedMonth(prevMonth => {
+      const newMonth = prevMonth + step;
+      if (newMonth < 0) {
+        setSelectedYear(prevYear => prevYear - 1);
+        return 11;
+      }
+      if (newMonth > 11) {
+        setSelectedYear(prevYear => prevYear + 1);
+        return 0;
+      }
+      return newMonth;
+    });
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Calendar</h2>
+        <button onClick={() => changeMonth(-1)}>Previous Month</button>
+        <button onClick={() => changeMonth(1)}>Next Month</button>
+      </div>
+      <div>
+        <h3>
+          {selectedYear} - {selectedMonth + 1}
+        </h3>
+        
+      </div>
+      
+    </div>
+  );
 }
 
 export default calendarFunction;
+
